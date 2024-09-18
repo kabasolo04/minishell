8d1a56e84f4a4e3135f6efab1e1a19ecccc77361
Markel Uribe Lasa <koldobikaabasolo@gmail.com>
2024-09-18 13:40:11 +0200

    cd OLDPWD fixesd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:34:13 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/09/16 13:14:57 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/09/18 13:31:19 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 void	built_cd(char *dir, int fd)
 {
 	char	*export;
+	char	*old;
 
+	old = getcwd(NULL, 0);
 	if (!dir || ft_strcmp(dir, "~") == 0)
 		dir = getenv("HOME");
 	if (chdir(dir) != 0)
@@ -27,6 +29,12 @@ void	built_cd(char *dir, int fd)
 		return ;
 	ft_strlcpy(export, "PWD=", 5);
 	ft_strlcat(export, dir, ft_strlen(dir) + ft_strlen(export) + 1);
+	built_export(export, fd);
+	export = malloc(sizeof(char) * (ft_strlen(old) + 8));
+	if (!export)
+		return ;
+	ft_strlcpy(export, "OLDPWD=", 8);
+	ft_strlcat(export, old, ft_strlen(old) + ft_strlen(export) + 1);
 	built_export(export, fd);
 	free(export);
 }
